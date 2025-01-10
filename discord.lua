287173357bb8a0853319b53887ea042987401fc4
❱•❰ Hjuiihu <83204744+Hjuiihu@users.noreply.github.com>
2025-01-10 19:35:42 +0100

    Add files via upload

---
+++ discord.lua
@@ -0,0 +1,25 @@
+Citizen.CreateThread(function()
+  while true do 
+  
+    local playerName = GetPlayerName(PlayerId())
+    local PlayerId = Getplayerserverid(PlayerId())
+
+    local appId = 1295327856447979631
+    local bigPic = 'big'
+
+    SetDiscordAppId(appId)
+
+    setRichPresence('Username: ' .. playerName .. 'Server Id:' .. playerId)
+
+    SetDiscordRichPresenceAsset(bigPic)
+    SetDiscordRichPresenceAssetText('Big Text')
+
+    SetDiscordRichPresenceAssetText('Small Text')
+    
+
+    setRichPresenceAction(0, 'Discord', 'https://discord.gg/gUAmHM33vq')
+    setRichPresenceAction(1, 'Join Server', 'https://cfx.re/join/dl9pe5')
+
+    Citizen.Wait(1000)
+  end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'hujiihu'
+
+games {'gta5'}
+
+client_script 'discord.lua'
